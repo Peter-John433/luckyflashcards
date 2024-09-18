bd62e814936fe5156888f59c67db748952439bca
iampeterjohn <peterjeche442@gmail.com>
2024-09-18 01:21:56 +0100

    flash cards setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import FlashCard from "./FlashCard";
+
+export default function App() {
+  return (
+    <div>
+      <FlashCard />
+    </div>
+  );
+}
---
+++ src/FlashCard.js
@@ -0,0 +1,7 @@
+export default function FlashCard() {
+  return (
+    <div>
+      <h3>hello world</h3>
+    </div>
+  );
+}
